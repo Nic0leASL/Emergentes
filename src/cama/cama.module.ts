37a3b0734ab4a082c5fbaea2a4a845dca7181f58
Angela Sanchez <sla0033391@est.univalle.edu>
2025-09-25 21:23:49 -0400

    Fix: Final import path corrections for Heroku build

--- src/cama/cama.module.ts
+++ src/cama/cama.module.ts
@@ -5,7 +5,7 @@ import { CamaService } from './cama.service';
 import { CamaController } from './cama.controller';
 import { Specialty } from '../specialty/specialty.entity';
 import { Servicio } from '../servicio/servico.entity';
-import { EstablecimientoSalud } from '../establecimiento/establecimiento.entity';
+import { EstablecimientoSalud } from 'src/establecimiento/establecimiento.entity';
 import { HistoriaCamaModule } from '../historial_cama/hostoria_cama.module'; 
 import { HistoriaCama } from '../historial_cama/historial_cama.entity';
 import {AuthModule} from '../Auth/auth.module';
